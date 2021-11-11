19b37567f2b85f10d302b51d427a19d739744c82
qhhivan <orsolic.i02@htlwienwest.at>
2021-11-11 13:01:57 +0100

    Navbar v1

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar color="black" absolute dark>
+    <!-- LOGO -->
+    <img src="../../public/img/logo.svg" width="55" height="55" class="my-1" />
+
+    <v-spacer></v-spacer>
+
+    <!-- Soll nur der Trainer sehen + on click soll ein neues Fenster geöfnet werden wo man Termine erstellen kann -->
+    <v-icon color="#009D2B" @click="createTraining()">{{
+      mdiCalendarPlus
+    }}</v-icon>
+  </v-app-bar>
+</template>
+
+<script>
+// ICON
+import { mdiCalendarPlus } from '@mdi/js';
+
+export default {
+  data() {
+    return {
+      mdiCalendarPlus: mdiCalendarPlus,
+    };
+  },
+
+  methods: {
+    createTraining() {
+      return alert('In Progress');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
