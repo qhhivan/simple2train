105ab33b594d75ae4b46dc63c4c0362d227acdc7
qhhivan <orsolic.i02@htlwienwest.at>
2021-12-14 09:20:59 +0100

    CLient

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,48 +1,42 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Profil from '../views/Profil.vue';
+import Kalender from '../views/Kalender.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home,
-  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/kalender',
-    name: 'Kalender',
-
-    component: () => import('../views/Kalender.vue'),
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
     path: '/profil',
     name: 'Profil',
-
-    component: () => import('../views/Profil.vue'),
+    component: Profil,
   },
   {
-    path: '/',
-    name: 'Termine',
-
-    component: () => import('../views/Termine.vue'),
+    path: '/kalender',
+    name: 'Kalender',
+    component: Kalender,
+  },
+  {
+    path: '/:trainingsID/details',
+    name: 'Details',
+    props: true,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Details.vue'),
   },
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,27 +1,56 @@
 <template>
-  <div class="container-fluid">
-    <v-row><Header /></v-row>
-    <!-- <v-row> <KWDatum /></v-row> -->
+  <v-container class="d-flex flex-wrap">
+    <v-card class="mx-auto ma-3" max-width="500" width="350" v-for="t of trainings" :key="t.trainings_id">
+      <v-card-text>
+        <div>
+          <v-icon class="mr-3" color="orange darken-2">{{ icon }}</v-icon>
+          <span class="text-h6 black--text">Training am: {{ t.datum.substring(0, 10) }}</span>
+        </div>
+        <div class="text--primary mt-5 ml-9">
+          <v-icon small>mdi-clock-time-nine-outline</v-icon
+          ><span class="ml-2">{{ t.beginn.substring(0, 5) }} - {{ t.ende.substring(0, 5) }}</span>
+        </div>
+        <div class="text--primary mt-1 ml-9">
+          <span class="font-weight-bold"> Anmeldefrist: </span>
+          <v-chip class="red white--text">{{ t.eintragefrist.substring(0, 10) }}</v-chip>
+        </div>
+        <div class="text--primary mt-1 ml-9">
+          <span class="font-weight-bold"> Kosten: </span>
+          {{ t.kosten }} €
+        </div>
+        <div class="text--primary mt-1 ml-9">
+          <span class="font-weight-bold"> Ort: </span>
+          {{ t.ort }}
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="green accent-4" :to="`/${t.trainings_id}/details`"> Details </v-btn>
+      </v-card-actions>
 
-    <!-- <termin /> -->
-    <!-- <menubar /> -->
-  </div>
+      <!-- <v-card-actions class="justify-center">
+        Button zum entscheiden ob man kommt oder nicht
+        <v-btn outlined rounded text> Ja </v-btn>
+        <v-btn outlined rounded text> Vlt </v-btn>
+        <v-btn outlined rounded text> Nein </v-btn>
+      </v-card-actions> -->
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import Header from '@/components/Header';
-// import KWDatum from '@/components/KWDatum.vue';
-// import Termin from '@/components/Termin.vue';
-// import Menubar from '@/components/Menubar.vue';
+import { mdiVlc } from '@mdi/js';
 
 export default {
   name: 'Home',
-
-  components: {
-    Header,
-    // KWDatum,
-    // Termin,
-    // Menubar,
+  props: {
+    trainings: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      icon: mdiVlc,
+    };
   },
 };
 </script>
